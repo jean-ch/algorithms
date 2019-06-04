3eb92c564eb17306f81cf75ced1e75ab452c404f
Jingxuan Chen <jingxuan0601@gmail.com>
2019-06-05 00:03:49 +0800

    Update Palindrome Partitioning II.js

--- Palindrome Partitioning II.js
+++ Palindrome Partitioning II.js
@@ -6,7 +6,9 @@
  * 2. 用dp求isPalindrome: dp[i][j]表示s[i, j]是否是palindrome
  * - 状态方程： dp[i][j] = dp[i + 1][j - 1] && s[i] === s[j]
  * - 因为状态方程中遍历到dp[i][j]之前应该先求得dp[i + 1][j - 1]的状态，所以是由中心点展开的一个遍历，外层的遍历参数相当于length
- * - 初始状态：dp[i][i] = true, dp[i][i + 1] = s[i] === s[j]。为什么dp[i][i + 1]也要在初始状态里算好呢，因为状态方程里面dp[i + 1][j - 1]要求j - i >= 2
+ * - 初始状态：dp[i][i] = true, dp[i][i + 1] = s[i] === s[j]。为什么dp[i][i + 1]也要在初始状态里算好呢，因为状态方程里面dp[i + 1][j - 1]要求j - i >= 2   
+ * 
+ * js整数最大数: Number.MAX_SAFE_INTEGER
  */
 
 /**
